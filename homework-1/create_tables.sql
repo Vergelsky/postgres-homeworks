14c4d3fbafe45bc61528bb15cae78145840f8eec
Dmitry Vergelsky <graf.werger@gmail.com>
2023-11-28 02:21:13 +0700

    Домашка 1 готова

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers_data
+(
+	customer_id VARCHAR(5) PRIMARY KEY,
+	company_name VARCHAR(100) NOT NULL,
+	contact_name VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE employees_data
+(
+	employee_id int PRIMARY KEY,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+)
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id VARCHAR(5) REFERENCES customers_data(customer_id) NOT NULL,
+	employee_id int REFERENCES employees_data(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city VARCHAR(100) NOT NULL
+)
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,44 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+import csv
+
+
+customers_url = os.path.join('north_data', 'customers_data.csv')
+employees_url = os.path.join('north_data', 'employees_data.csv')
+orders_data_url = os.path.join('north_data', 'orders_data.csv')
+
+data_urls = [customers_url, employees_url, orders_data_url]
+
+
+def create_list_from_csv(url_):
+    """
+    Первым возвращает количество столбцов.
+    Вторым возвращает список кортежей значений.
+    :url_: путь до csv файла
+    :return: Список кортежей элементов строк csv таблицы, кол-во столбцов.
+    """
+    with open(url_, 'r') as file:
+        list_ = [tuple(line) for line in csv.reader(file)]
+        return len(list_[0]), list_[1:]
+
+
+north_conn = psycopg2.connect(host='localhost',
+                              database='north',
+                              user='postgres',
+                              password='5455')
+
+
+try:
+    with north_conn:
+        with north_conn.cursor() as cursor:
+            #  для каждого адреса
+            for url in data_urls:
+                #  создаём список атрибутов элементов
+                col, data = create_list_from_csv(url)
+                #  и записываем его в таблицу с соответствующим названием
+                num_of_params = ', '.join(['%s' for n in range(col)])
+                for line in data:
+                    cursor.execute(f"INSERT INTO {os.path.basename(url)[:-4]} VALUES ({num_of_params})", line)
+finally:
+    north_conn.close()
